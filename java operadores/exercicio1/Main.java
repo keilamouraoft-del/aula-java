281d2e20c89a2b8e9a2b89c5ae0fc335c24bb59b
Keila <Keila.murilo@hotmail.com>
2026-08-23 19:25:36 -0300

    Adiciona primeiro exercicio java

---
+++ java operadores/exercicio1/Main.java
@@ -0,0 +1,10 @@
+        class main{
+    
+    public static void main(String[] args) {
+    double base = 2.00;
+    double altura = 2.20;
+    double total = base*altura;
+    
+    System.out.println(total);
+    }
+}
