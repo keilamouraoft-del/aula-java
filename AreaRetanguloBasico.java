ac193cfd126cf4261d3a3deb9c3e5f23690319f1
Keila <Keila.murilo@hotmail.com>
2026-08-27 15:58:42 -0300

    Adicione exercício básico de área do retângulo

---
+++ AreaRetanguloBasico.java
@@ -0,0 +1,10 @@
+public class AreaRetanguloBasico {
+    
+    public static void main(String[] args) {
+    double base = 2.00;
+    double altura = 2.20;
+    double total = base*altura;
+    
+    System.out.print("A área total é: " + total);
+    }
+}
